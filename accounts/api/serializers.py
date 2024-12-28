0cfa1ac8cd8641f1e9dfe17f106f1931831f2d51
Xue Yang <>
2024-12-27 18:15:10 -0800

    Login, Login Status, Logout, Sign up API

---
+++ accounts/api/serializers.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers, exceptions
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'User does not exist.'
+            })
+        return data
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(min_length=6, max_length=20)
+    password = serializers.CharField(min_length=6, max_length=20)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+    # will be called when is_valid is called
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'The username has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'The email has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+        user = User.objects.create_user(
+            username = username,
+            password = password,
+            email = email,
+        )
+        return user
