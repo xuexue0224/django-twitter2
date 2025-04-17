90f6b3701e1779126cebe6e2b51b961277634b1a
Xue Yang <>
2025-04-16 17:30:08 -0700

    5-accounts-api-unit-tests

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import permissions
 from rest_framework.decorators import action
 from accounts.api.serializers import UserSerializer, LoginSerializer, SignupSerializer
@@ -40,13 +40,13 @@ class AccountViewSet(viewsets.ViewSet):
 
     @action(methods=['POST'], detail=False)
     def login(self, request):
-        serializer = LoginSerializer(data = request.data)
+        serializer = LoginSerializer(data=request.data)
         if not serializer.is_valid():
             return Response({
                 "success": False,
                 "message": "Please check input",
                 "errors": serializer.errors,
-            }, status = 400)
+            }, status=status.HTTP_400_BAD_REQUEST)
         username = serializer.validated_data['username']
         password = serializer.validated_data['password']
         user = django_authenticate(username = username, password = password)
@@ -54,7 +54,7 @@ class AccountViewSet(viewsets.ViewSet):
             return Response({
                 "success": False,
                 "message": "username and password does not match",
-            }, status = 400)
+            }, status=status.HTTP_400_BAD_REQUEST)
         django_login(request, user)
         return Response({
             "success": True,
