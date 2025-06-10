9243deacfda2d03fb5a09f4f534cff1182923c4e
Xue Yang <>
2025-06-10 09:23:41 -0700

    24-photos-model-and-admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from tweets.models import Tweet
+from tweets.models import Tweet, TweetPhoto
 from datetime import timedelta
 from utils.time_helpers import utc_now
 from testing.testcases import TestCase
+from tweets.constants import TweetPhotoStatus
 
 # Create your tests here.
 
@@ -38,3 +39,13 @@ class TweetTests(TestCase):
         dongxie = self.create_user('dongxie')
         self.create_like(dongxie, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_create_photo(self):
+        # 测试可以成功创建 photo 的数据对象
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.linghu,
+        )
+        self.assertEqual(photo.user, self.linghu)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
